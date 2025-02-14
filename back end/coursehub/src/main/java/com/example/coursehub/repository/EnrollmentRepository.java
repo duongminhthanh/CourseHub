38746edd56c03f385f1a9fa290cce5635f20bf3a
duongminhthanh <duongminhthanh1999td@gmail.com>
2025-02-14 08:10:26 +0700

    fix EnrollmentRepository

--- back end/coursehub/src/main/java/com/example/coursehub/repository/EnrollmentRepository.java
+++ back end/coursehub/src/main/java/com/example/coursehub/repository/EnrollmentRepository.java
@@ -1,9 +1,10 @@
 package com.example.coursehub.repository;
 
+import com.example.coursehub.entities.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnrollmentRepository extends JpaRepository<EnrollmentRepository,Long> {
+public interface EnrollmentRepository extends JpaRepository<Enrollment,Long> {
 
 }
